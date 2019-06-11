6c1498cf3dc41e05bbc664862b99bddf1cb411b0
BlackBeans <adrien.mathieu.net@gmail.com>
2019-06-11 12:55:21 +0200

    [main.py] Done a first sketch of a console which returns the AST of the input code (or the error message) instead of executing it

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import beansast, beansexe, beanscomp
+#!/usr/bin/python3
 
+from beansast import ASTBuilder
+from beansast.psrgmrparser import ASTNode
+from beansast.stdout import print_tree
+from beansast.color import *
 
+PS1 = BLUE + BOLD + "bns> " + ENDC
+
+inp = input(PS1)
+
+while inp not in {"q", "e", "quit", "exit"}:
+    try:
+        print_tree(ASTBuilder(inp).get_ast())
+    except SystemExit:
+        pass
+    inp = input(PS1)

59513a15b0778e9e766bacb1cd4ba0645c6ceef2
BlackBeans <adrien.mathieu.net@gmail.com>
2020-03-15 19:10:42 +0100

    [test.py] .

--- test.py
+++ test.py
@@ -1,11 +1,50 @@
 #!/usr/bin/python3
 
-from beansast import ASTBuilder
-from beansast.stdout import print_tree
+from beansast import psrgmrparser, lexer
+from bininterface import *
 
-FILE = "utest_scripts/test1.bns"
+proxy = OrderedDict(
+    String(),                # key
+    Struct(
+        String(),            # value
+        Bool(),              # list_append
+        Int(),               # flags
+        String()             # type
+    )
+)
 
-with open(FILE) as f:
-    inp = f.read()
+rule_token = Struct(
+    String(),                # name
+    String(),                # attribute
+    Bool(),                  # is_append
+    String()                 # key
+)
 
-print_tree(ASTBuilder(inp).get_ast())
+rule = Struct(
+    List(rule_token),
+    proxy
+)
+
+trie_node = Struct(
+    List(rule),              # rules
+    Int(),                   # id
+    Dict(                    # children
+        rule_token,          # token
+        Int()                # child id
+    )
+)
+trie = List(
+    trie_node
+)
+
+with open("beansast/gmrs/plexer-m.gmr") as f:
+    grammar = lexer.Lexer(f.read(), file="beansast/gmrs/plexer-m.gmr", helperr=True)
+
+with open("beansast/gmrs/parser.gmr") as f:    
+    grammar(f.read(), fn="beansast/gmrs/parser.gmr")
+
+a = psrgmrparser.ParserReader(grammar,helperr=True)
+nodizers = a.read()    
+
+with open("parser.bo", "wb") as f:
+    f.write(nodizers.compile())

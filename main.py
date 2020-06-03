e590591711a59b1eff1e779f0fb6dce26d14e17d
BlackBeans <adrien.mathieu.net@gmail.com>
2020-06-03 12:07:24 +0200

    [main] Added DEBUG flag to mesure time spent on each execution step

--- main.py
+++ main.py
@@ -2,17 +2,36 @@
 from beansexe import executer
 from beansast import ASTBuilder
 from beanscomp import vmcompiler
-import sys
+import sys, time
 
+DEBUG = False
+
+def debug(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
+debug('Building grammar...', end=" ")
+t_grammar_1 = time.time()*1000
 parser = ASTBuilder()
+t_grammar_2 = time.time()*1000
+debug('done.')
 ex = executer.Executer()
 compiler = vmcompiler.VMCompiler()
 
+debug('Building ast...', end=" ")
+t_ast_1 = time.time()*1000
 with open(sys.argv[1]) as f:
     ast = parser.get_ast(f.read(), sys.argv[1])
+t_ast_2 = time.time()*1000
+debug("done")
 
 #compiler.compile(ast)
-    
+debug("Executing...", end=" ")
+t_exec_1 = time.time()
 a = ex.exec(ast)
+t_exec_2 = time.time()
+debug("done.")
 if a != None:
     print("(%s) %s" % tuple(a))
+
+debug('Times:\n Grammar: %sms\n AST: %sms\n Exec: %sms' % (round(t_grammar_2 - t_grammar_1), round(t_ast_2 - t_ast_1), round(t_exec_2 - t_exec_1)))
